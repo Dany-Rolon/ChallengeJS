634b9d1cf732c17df6ad7ffa4341d5df81a21777
Dany-Rolon <dany_rolon@outlook.com>
2021-04-30 18:07:26 -0300

    feat: Add personalize colors to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
    darkMode: false, // or 'media' or 'class'
    theme: {
-     extend: {},
+     extend: {
+       colors:{
+         'void': "#0e0B16",
+         'fuschia': "#a239ca",
+         'jewel': "#4717f6",
+         'stark': "#e7dfdd"
+       }
+     },
    },
    variants: {
      extend: {},
