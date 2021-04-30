4681161841d96b8379fc977ac3ae2ed6bf63989d
Dany-Rolon <dany_rolon@outlook.com>
2021-04-30 12:55:47 -0300

    feat: Add nav component render and switch for routes

--- client/src/views/AuthenticateUser.view.jsx
+++ client/src/views/AuthenticateUser.view.jsx
@@ -1,7 +1,17 @@
-export default function AuthenticateUserView(){
-    return(
+import { Route, Switch } from 'react-router-dom'
+
+import Home from './Home.view'
+import Operations from './Operations.view'
+import Nav from '../components/Nav'
+
+export default function AuthenticateUserView() {
+    return (
         <div>
-            AuthenticateUserView
+            <Nav />
+            <Switch>
+                <Route path="/" component={Home} />
+                <Route path="/operations" component={Operations} />
+            </Switch>
         </div>
     )
 }
