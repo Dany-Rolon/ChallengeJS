22ce0b756259cb49dcb35ab0c595ab4c7683c437
Dany-Rolon <dany_rolon@outlook.com>
2021-04-30 18:05:41 -0300

    feat: Add operations list and current balance components and dummy operations

--- client/src/views/Home.view.jsx
+++ client/src/views/Home.view.jsx
@@ -1,7 +1,95 @@
+import {useState} from 'react'
+import OperationList from '../components/OperationList'
+import CurrentBalance from '../components/CurrentBalance'
+
 export default function Home(){
+
+    const [data, setData] = useState([
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'expense',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'income',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'expense',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'expense',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'expense',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'expense',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'income',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'income',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'income',
+            category: 'Job'
+        },
+        {
+            concept: 'Cobre mi sueldo',
+            mount: 60000,
+            date: '30 abr 2021',
+            type: 'income',
+            category: 'Job'
+        },
+    ])
+
+    function getCurrentBalance(){
+        let total=0
+        data.forEach(item => {
+            if(item.type==="ingress") total += item.mount
+            else total -= item.mount
+        })
+        return total
+    }
+
     return(
         <div>
-            Home
+            <OperationList data={data}/>
+            <CurrentBalance balance={getCurrentBalance()}/>
         </div>
     )
 }
