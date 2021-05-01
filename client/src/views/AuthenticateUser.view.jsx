13f30ec7556082891e65a8d9bc7fcf238b959729
Dany-Rolon <dany_rolon@outlook.com>
2021-05-01 10:43:03 -0300

    feat: Add create operation route

--- client/src/views/AuthenticateUser.view.jsx
+++ client/src/views/AuthenticateUser.view.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom'
 import Home from './Home.view'
 import Operations from './Operations.view'
 import Nav from '../components/Nav'
+import CreateOperationCard from '../components/CreateOperationCard'
 
 export default function AuthenticateUserView() {
     return (
         <div>
             <Nav />
             <Switch>
+                <Route path="/create_operation" component={CreateOperationCard}/>
                 <Route path="/operations" component={Operations} />
                 <Route path="/" component={Home} />
             </Switch>
