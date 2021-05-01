17a161669570eb4e3be8acd7dc205344ef615cb9
Dany-Rolon <dany_rolon@outlook.com>
2021-05-01 10:42:40 -0300

    feat: Add prop options
    Options prop will tell component if buttons should render

--- client/src/components/OperationList.jsx
+++ client/src/components/OperationList.jsx
@@ -1,12 +1,12 @@
 import OperationCard from './OperationCard'
 
-export default function OperationList({data}){
+export default function OperationList({data, options}){
 
     return(
         <section>
             <ul className="container mx-auto grid grid-cols-1">
                 {
-                    data.map(operation => <OperationCard  operation={operation}/>)
+                    data.map(operation => <OperationCard  operation={operation} options={options}/>)
                 }
             </ul>
         </section>
