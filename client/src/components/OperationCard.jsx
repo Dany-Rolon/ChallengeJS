d703f01c2802316d3b63fd2a2c57d8f1aca7047c
Dany-Rolon <dany_rolon@outlook.com>
2021-05-03 16:05:21 -0300

    feat: Change colors and conditionally show Edit and Delete buttons

--- client/src/components/OperationCard.jsx
+++ client/src/components/OperationCard.jsx
@@ -1,22 +1,26 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function OperationCard({ operation, options }) {
     return (
-        <li className="p-4 m-4 border-2 border-stark text-white md:mx-72">
-            <h6 className="font-semibold text-xl text-stark">{operation.concept}</h6>
+        <li className="p-4 m-4 bg-bg_2 shadow-md">
+            <h6 className="font-semibold text-xl text-primary">{operation.concept}</h6>
             <div className="flex flex-wrap justify-between">
-                <h6><b className="text-fuschia">Mount:</b> {operation.mount}</h6>
-                <h6 ><b className="text-fuschia">Type:</b><p className={`${operation.type==='income' ? 'text-green-500' : 'text-red-500'} inline`}> {operation.type}</p>
+                <h6><b className="text-primary">Mount:</b> {operation.mount}</h6>
+                <h6 ><b className="text-primary">Type:</b><p className={`${operation.type === 'income' ? 'text-green-600' : 'text-red-600'} inline`}> {operation.type}</p>
                 </h6>
             </div>
             <div className="flex flex-wrap justify-between">
-                <h6><b className="text-fuschia">Date:</b> {operation.date}</h6>
-                <h6><b className="text-fuschia">Category:</b> {operation.category}</h6>
+                <h6><b className="text-primary">Date:</b> {operation.date}</h6>
+                <h6><b className="text-primary">Category:</b> {operation.category}</h6>
             </div >
 
-            <div className="flex mt-4 justify-evenly">
-                    <Link to="/edit_operation" className="px-2 border-2 border-jewel text-jewel text-xl font-bold hover:bg-jewel hover:text-stark">Edit</Link>
-                    <button className="px-2 border-2 border-fuschia text-fuschia text-xl font-bold hover:bg-fuschia hover:text-stark">Delete</button>
-                </div>
+            {
+                options ? (
+                    <div className="flex mt-4 justify-evenly">
+                        <Link to={{ pathname: "/edit_operation", item: operation }} className="px-2 border-2 border-secondary text-primary text-xl font-bold hover:bg-secondary hover:text-bg_1">Edit</Link>
+                        <button className="px-2 border-2 border-secondary text-primary text-xl font-bold hover:bg-secondary hover:text-bg_1">Delete</button>
+                    </div>
+                ) : null
+            }
         </li>
     )
 }
