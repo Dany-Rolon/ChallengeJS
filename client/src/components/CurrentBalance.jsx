f6270e050237ab158fca82d96cb71b428b0e1d54
Dany-Rolon <dany_rolon@outlook.com>
2021-05-03 16:03:35 -0300

    fix: change colors to make them look better

--- client/src/components/CurrentBalance.jsx
+++ client/src/components/CurrentBalance.jsx
@@ -1,8 +1,8 @@
 export default function CurrentBalance({balance}){
 
     return(
-        <footer className="w-screen py-4 bg-jewel text-center text-white font-semibold sticky bottom-0">
-            <h1 className="text-xl"> Current balance:<p className={`${balance<0 ? 'text-red-300' : 'text-green-300'}`}> ${balance} </p></h1>
+        <footer className="w-screen py-4 bg-secondary text-center text-primary font-semibold sticky bottom-0">
+            <h1 className="text-xl"> Current balance:<p className={`${balance<0 ? 'text-red-600' : 'text-green-600'}`}> ${balance} </p></h1>
         </footer>
     )
 } 
