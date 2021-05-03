132e8ef7427c7917b299ff2e0b928aca776b28e8
Dany-Rolon <dany_rolon@outlook.com>
2021-05-03 16:04:29 -0300

    feat: Add responsive features for menu

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,35 +3,51 @@ import { Menu } from '@headlessui/react'
 
 export default function Nav() {
     return (
-        <nav className="flex justify-evenly p-4 bg-fuschia w-screen">
-            <h1 className="font-bold text-xl text-stark">My budgets</h1>
-            <MenuComponent />
+        <nav className="flex justify-evenly p-4 bg-secondary w-screen">
+            <h1 className="font-bold text-xl text-primary">My budgets</h1>
+            <MenucomponentDesktop />
+            <MenuComponentMobile />
         </nav>
     )
 }
 
-function MenuComponent() {
+function MenucomponentDesktop() {
     return (
-        <Menu as="div" className="grid grid-cols-1 place-items-end">
+        <div className="hidden md:flex">
+            <Link to="/"><p className="text-primary font-semibold pr-3 hover:underline">Home</p></Link>
+            <Link to="/operations"><p className="text-primary font-semibold pr-3 hover:underline">Budgets</p></Link>
+            <p className="text-primary font-semibold hover:underline">Sing out</p>
+        </div>
+    )
+}
+
+function MenuComponentMobile() {
+    return (
+        <Menu as="div" className="flex-row text-right md:hidden">
             <Menu.Button>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-stark" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </Menu.Button>
-            <Menu.Items className="grid grid-cols-1 place-items-end gap gap-y-1 mt-2 text-stark">
+            <Menu.Items className="mt-2 text-stark"> 
+            {/* grid grid-cols-1 place-items-end gap gap-y-1 */}
                 <Menu.Item className="hover:underline font-bold">
                     {({ active }) => (
-                        <Link to="/" as="a">Home</Link>
+                        <Link to="/">
+                            <p className="text-primary">Home</p>
+                        </Link>
                     )}
                 </Menu.Item>
                 <Menu.Item className="hover:underline font-bold">
                     {({ active }) => (
-                        <Link to="/operations" as="a">Operations</Link>
+                        <Link to="/operations">
+                            <p className="text-primary">Budgets</p>
+                        </Link>
                     )}
                 </Menu.Item>
                 <Menu.Item className="hover:underline font-bold cursor-pointer">
                     {({ active }) => (
-                        <p>Sing out</p>
+                        <p className="text-primary">Sing out</p>
                     )}
                 </Menu.Item>
             </Menu.Items>
