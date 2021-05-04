fb4c96f635c575e8badc6e4e52a02860981e8b3e
Dany-Rolon <dany_rolon@outlook.com>
2021-05-04 13:57:29 -0300

    Create user.errors.js

---
+++ server/api/resources/user/user.errors.js
@@ -0,0 +1,22 @@
+class UserDataAlreadyInUse extends Error {
+    constructor(message){
+        super(message)
+        this.message = message || 'The email is already associated with an account'
+        this.status = 409
+        this.name = 'UserDataAlreadyInUse'
+    }
+}
+
+class WrongCredentials extends Error {
+    constructor(message){
+        super(message)
+        this.message = Message || 'Wrong credentials'
+        this.status = 400
+        this.name = 'WrongCredentials'
+    }
+}
+
+module.exports = {
+    UserDataAlreadyInUse,
+    WrongCredentials
+}
