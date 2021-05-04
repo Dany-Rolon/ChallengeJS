f225a64c39da9690a179ef3509ce80c7a07291ce
Dany-Rolon <dany_rolon@outlook.com>
2021-05-04 10:46:55 -0300

    style: General changes

--- client/src/views/NonAuthenticateUser.view.jsx
+++ client/src/views/NonAuthenticateUser.view.jsx
@@ -21,38 +21,38 @@ export default function NonAuthenticateUserView(){
 
     return(
         <div className="grid grid-cols-1">
-            <h1 className="text-primary text-center text-4xl mt-20 font-semibold">My Budgets</h1>
-            <form onSubmit={onSubmit} className="grid grid-cols-1 border-2 border-secondary shadow-lg p-2 mt-14 m-2" >
+            <h1 className="text-primary text-center text-4xl mt-20 font-semibold title md:italic">My Budgets</h1>
+            <form onSubmit={onSubmit} className="grid grid-cols-1 border-2 border-secondary shadow-lg p-2 mt-10 m-2 rounded-lg pt-4" >
                 {
-                    isLogin ? (null) : (<h2 className="text-2xl text-primary text-center">Create Account</h2>)
+                    isLogin ? (<h2 className="text-3xl text-primary text-center title">Login</h2>) : (<h2 className="text-3xl text-primary text-center title">Create Account</h2>)
                 }
                 
-                <h3 className="text-secondary font-bold">Email</h3>
+                <h3 className="text-secondary font-bold pt-3">Email</h3>
                 <input type="text" className="w-full pl-1"/>
 
-                <h3 className="text-secondary font-bold">Password</h3>
+                <h3 className="text-secondary font-bold pt-3">Password</h3>
                 <input type="password" className="w-full pl-1"/>
 
                 {
                     isLogin ? null : (
                         <div>
-                            <h3 className="text-secondary font-bold">Repeat Password</h3>
+                            <h3 className="text-secondary font-bold pt-3">Repeat Password</h3>
                             <input type="password" className="w-full pl-1"/>
                         </div>
                     )
                 }
                 
-                <button type="submit" className="border-2 border-secondary text-primary font-semibold py-2 mt-2 hover:bg-secondary hover:text-white">{isLogin ? 'Login' : 'Register'}</button>
+                <button type="submit" className="border-2 border-secondary text-primary py-2 mt-4 rounded-lg text-2xl title hover:bg-secondary hover:text-white">{isLogin ? 'Submit' : 'Register'}</button>
             </form>
 
             {
                 isLogin ?
                     (<div className="text-center">
-                        <p>Don't have an account yet? <button className="text-secondary focus:outline-none" onClick={onClick}>Register</button></p> 
+                        <p>Don't have an account yet? <button className="text-secondary mt-4 underline focus:outline-none" onClick={onClick}>Register</button></p> 
                     </div>)
                 :
                     (<div className="text-center">
-                        <p>Already have an account?  <button className="text-secondary focus:outline-none" onClick={onClick}>Login</button></p>
+                        <p>Already have an account?  <button className="text-secondary mt-4 underline focus:outline-none" onClick={onClick}>Login</button></p>
                     </div>)
             }
 
