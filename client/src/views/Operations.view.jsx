91fab22d62e431f5d323b6f96d263b4847f3ad12
Dany-Rolon <dany_rolon@outlook.com>
2021-05-04 10:48:02 -0300

    style: Change some sizes and positions

--- client/src/views/Operations.view.jsx
+++ client/src/views/Operations.view.jsx
@@ -1,6 +1,6 @@
 import OperationList from '../components/OperationList'
 import { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Operations() {
 
@@ -10,7 +10,7 @@ export default function Operations() {
             concept: 'Cobre mi sueldo',
             mount: 60000,
             date: '30 abr 2021',
-            type: 'expense',
+            type: 'income',
             category: 'Job'
         },
         {
@@ -31,21 +31,21 @@ export default function Operations() {
             concept: 'Cobre mi sueldo',
             mount: 60000,
             date: '30 abr 2021',
-            type: 'expense',
+            type: 'income',
             category: 'Job'
         },
         {
             concept: 'Cobre mi sueldo',
             mount: 60000,
             date: '30 abr 2021',
-            type: 'expense',
+            type: 'income',
             category: 'Job'
         },
         {
             concept: 'Cobre mi sueldo',
             mount: 60000,
             date: '30 abr 2021',
-            type: 'expense',
+            type: 'income',
             category: 'Job'
         },
         {
@@ -98,28 +98,31 @@ export default function Operations() {
     return (
         <div className="text-primary">
             <Menu />
-            <OperationList data={filterData} options={true}/>
+            <OperationList data={filterData} options={true} />
         </div>
     )
 
     function Menu() {
         return (
             <div className="w-full py-4 bg-bg_1 text-center font-semibold sticky top-0">
-                <div className="flex justify-evenly content-center|">
+                <div className="flex  justify-between mx-4 md:mx-8">
                     <div className="flex">
-                        <h3 className="mr-1">Filter by:</h3>
+                        <h3 className="mr-1 text-xl">Filter by:</h3>
                         <div onClick={onClick} className="flex cursor-pointer">
-                            <h4>{type}</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 self-center mb-1 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <h4 className="text-xl">{type}</h4>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 self-center mb-1 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                             </svg>
                         </div>
                     </div>
-                    <Link to="/create_operation">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                    </Link>
+                    <div className="flex">
+                        <h4 className="text-xl">Create</h4>
+                        <Link to="/create_operation">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </Link>
+                    </div>
                 </div>
             </div>
         )
