c7189a3cc60d66dc11fca75403c4e9cf66dde627
Dany-Rolon <dany_rolon@outlook.com>
2021-05-04 10:40:59 -0300

    style: Change size, font and color for 'Current balance' text

--- client/src/components/CurrentBalance.jsx
+++ client/src/components/CurrentBalance.jsx
@@ -2,7 +2,7 @@ export default function CurrentBalance({balance}){
 
     return(
         <footer className="w-screen py-4 bg-secondary text-center text-primary font-semibold sticky bottom-0">
-            <h1 className="text-xl"> Current balance:<p className={`${balance<0 ? 'text-red-600' : 'text-green-600'}`}> ${balance} </p></h1>
+            <h1 className="text-2xl title text-primary"> Current balance:<p className={`${balance<0 ? 'text-red-600' : 'text-green-600'}`}> ${balance} </p></h1>
         </footer>
     )
 } 
