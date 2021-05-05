97181cc71987db18c87681ea69d0bca1dc13151f
Dany-Rolon <dany_rolon@outlook.com>
2021-05-05 11:17:27 -0300

    feat: Add db connection and passport config

--- server/index.js
+++ server/index.js
@@ -2,13 +2,19 @@ const express = require('express')
 const log = require('./utils/logger')
 const app = express()
 const cors = require('cors')
+const passport = require('passport')
+const authJWT = require('./api/libs/auth')
 
 const UserRoute = require('./api/resources/user/user.routes')
 
-require('./db/connection')
+//Connection to DB
+require('./db/db')
 
 app.use(cors())
 app.use(express.json())
+
+passport.use(authJWT)
+app.use(passport.initialize())
 
 app.use('/user', UserRoute)
 
