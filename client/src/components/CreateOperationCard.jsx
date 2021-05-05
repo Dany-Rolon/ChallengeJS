34133a167715f8f26b22a2de2ac7a6fd3348dd36
Dany-Rolon <dany_rolon@outlook.com>
2021-05-05 11:13:04 -0300

    style: Show buttons in a grid

--- client/src/components/CreateOperationCard.jsx
+++ client/src/components/CreateOperationCard.jsx
@@ -29,9 +29,9 @@ export default function CreateOperationCard(props) {
 
     return (
         <div>
-            <h3 className="text-4xl text-center text-stark font-bold pt-10">{item ? 'Edit Operation' : 'Create Operation'}</h3>
+            <h3 className="text-4xl text-center text-stark font-bold pt-10 title">{item ? 'Edit Operation' : 'Create Operation'}</h3>
 
-            <form className="mx-5 p-4 border-2 shadow-md border-secondary mt-14 md:w-80 md:mt-28 sm:container sm:mx-auto">
+            <form className="mx-5 p-4 border-2 shadow-lg rounded-lg border-secondary mt-14 md:w-80 md:mt-28 sm:container sm:mx-auto">
 
                 <h6 className="font-bold text-fuschia">Concept:</h6>
                 <input className="bg-void border-b-2 border-stark text-stark w-full pl-0.5 focus:outline-none" type="text" value={operation.concept} onChange={onChangeHandler} name="concept" />
@@ -55,9 +55,9 @@ export default function CreateOperationCard(props) {
                 <h6 className="font-bold text-fuschia pt-1">Category:</h6>
                 <input className="bg-void border border-stark text-stark w-full pl-0.5 focus:outline-none" type="text" value={operation.category} onChange={onChangeHandler} name="category" />
 
-                <div className="flex mt-4 justify-evenly">
-                    <Link to="/operations" className="px-2 border-2 border-secondary text-jewel text-xl font-bold hover:bg-secondary hover:text-bg_1">Cancel</Link>
-                    <button className="px-2 border-2 border-secondary text-fuschia text-xl font-bold hover:bg-secondary hover:text-bg_1">{item ? 'Edit' : 'Create'}</button>
+                <div className="grid grid-cols-2 mt-5">
+                    <Link to="/operations" className="text-center px-2 border-2 border-secondary text-jewel text-xl font-bold rounded-lg title hover:bg-secondary hover:text-bg_1">Cancel</Link>
+                    <button className="px-2 border-2 border-secondary text-fuschia text-xl font-bold rounded-lg title hover:bg-secondary hover:text-bg_1">{item ? 'Edit' : 'Create'}</button>
                 </div>
             </form>
         </div>
