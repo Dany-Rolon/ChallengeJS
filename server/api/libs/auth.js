99ea06a55ed64bcdf85b59c8b01f129488bde3e3
Dany-Rolon <dany_rolon@outlook.com>
2021-05-05 11:15:34 -0300

    Create auth.js

---
+++ server/api/libs/auth.js
@@ -0,0 +1,26 @@
+const log = require('./../../utils/logger')
+const passportJWT = require('passport-jwt')
+const userController = require('./../resources/user/user.controller')
+
+let jwtOptions = {
+    secretOrKey: 'mybudgetSecret',
+    jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken()
+}
+
+module.exports = new passportJWT.Strategy(jwtOptions, async(jwtPayload, next) => {
+    try {
+        let user = await userController.getUser({id: jwtPayload.id})
+        if(!user){
+            log.warn('Invalid JWT Token')
+            next(null, false)
+            return
+        }
+        log.info(`Valid token. Authentication complete`)
+        next(null,{
+            email: user.email
+        })
+    } catch (error) {
+        log.error(`Error happened trying to authenticate ${error}`)
+        next(error)
+    }
+})
