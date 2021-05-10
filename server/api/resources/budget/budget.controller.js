d1015693e9395fbfef5ba8cb9d63dd80aa179e3d
Dany-Rolon <dany_rolon@outlook.com>
2021-05-10 15:01:38 -0300

    feat: Add editBudget and deleteBudget functions

--- server/api/resources/budget/budget.controller.js
+++ server/api/resources/budget/budget.controller.js
@@ -1,3 +1,4 @@
+const log = require('../../../utils/logger')
 const {Budget} = require('../../../db/db')
 
 async function createBudget(item, userID){
@@ -8,6 +9,33 @@ async function createBudget(item, userID){
     return newBudget
 }
 
+async function getBudgets(userID){
+    const result = await Budget.findAll({where:{user_id : userID}})
+    return result
+}
+
+async function editBudget(id, item){
+    await Budget.update(
+        {...item},
+        {where: {id}}
+    ).then(result => {
+        return result
+    }).catch(err => {
+        log.info(err)
+    })
+}
+
+async function deleteBudget(id){
+    Budget.destroy({
+        where:{
+            id
+        }
+    })
+}
+
 module.exports = {
-    createBudget
+    createBudget,
+    getBudgets,
+    editBudget,
+    deleteBudget
 }
