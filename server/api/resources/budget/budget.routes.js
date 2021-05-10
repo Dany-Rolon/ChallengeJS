3a1d13cc4a36bb81fa671377270d5ba92d17f1cf
Dany-Rolon <dany_rolon@outlook.com>
2021-05-10 15:02:02 -0300

    feat: Add edit and deleted methods for budgets

--- server/api/resources/budget/budget.routes.js
+++ server/api/resources/budget/budget.routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const log = require('../../../utils/logger')
 const passport = require('passport')
 const errorHandler = require('../../libs/errorHandler').handleErrors
 
@@ -15,7 +16,31 @@ bugdetRouter.post('/', [jwtAuthenticate,validateBudget], errorHandler(async(req,
     let newBudget = req.body
     let userID = req.user.id
     let response = await budgetController.createBudget(newBudget, userID)
-    console.log(response)
+    log.info(`A budget was created`)
+    res.status(200).json(response).end()
+}))
+
+//Send users budgets route
+bugdetRouter.get('/', jwtAuthenticate, errorHandler(async(req,res) => {
+    let userID = req.user.id
+    let result = await budgetController.getBudgets(userID)
+    res.status(200).json(result)
+}))
+
+//Edit a budget route
+bugdetRouter.put('/edit/:id', [jwtAuthenticate, validateBudget], errorHandler(async(req,res) => {
+    let budgetID = req.params.id
+    let editedBudget = req.body
+    await budgetController.editBudget(budgetID,editedBudget)
+    log.info(`Budget with id: ${budgetID} was edited`)
+    res.status(200).send('Budget edited')
+}))
+
+//Delete a budget route
+bugdetRouter.delete('/delete/:id', jwtAuthenticate, errorHandler(async (req,res) => {
+    let budgetID = req.params.id
+    await budgetController.deleteBudget(budgetID)
+    res.status(200).send('The budget was deleted')
 }))
 
 module.exports = bugdetRouter
