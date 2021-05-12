1dc3d9b14f9d7dee8135d80e3cc7156dcdb4ae2c
Dany-Rolon <dany_rolon@outlook.com>
2021-05-12 16:11:18 -0300

    feat: Add sign out button

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,20 @@
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
+import {deleteToken} from '../Helpers/auth-helper'
 
 export default function Nav(){
+    const history = useHistory()
+
+    function onClick(e){
+        e.preventDefault()
+        deleteToken()
+        history.push('/')
+    }
+
     return(
         <div>
             <Link to="/home">Home</Link>
             <Link to="/budgets">Budgets</Link>
+            <button onClick={onClick}>SingOut</button>
         </div>
     )
 }
