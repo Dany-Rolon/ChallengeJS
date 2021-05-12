8b2b2b1dbd9ca116511254f2a72d530e5e958f4c
Dany-Rolon <dany_rolon@outlook.com>
2021-05-12 15:05:12 -0300

    Update LandingPage.jsx

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,15 +1,15 @@
-import {Link} from 'react-router-dom'
+import { React } from 'react'
+import { Link } from 'react-router-dom'
 
-function LandingPage() {
+export default function LandingPage() {
+
   return (
     <div>
-        <h1>Landing Page</h1>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+      <h1>Landing Page</h1>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
     </div>
-
-  );
+  )
 }
 
-export default LandingPage;
 
