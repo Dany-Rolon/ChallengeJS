570023e525e0dcf1fd23f8b59c4880e0f3ee940d
Dany-Rolon <dany_rolon@outlook.com>
2021-05-12 13:25:43 -0300

    Create Index.js reducer

---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux'
+import {budgetReducer, selectedBudgetReducer} from './budgetsReducers'
+import {userReducer} from './userReducers'
+
+const reducers = combineReducers({
+    allBudgets: budgetReducer,
+    budgetSelected: selectedBudgetReducer,
+    user: userReducer
+})
+
+export default reducers
