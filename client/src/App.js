e8bf57b6f32b9f1d6f59dfb362e0095ad356680f
Dany-Rolon <dany_rolon@outlook.com>
2021-05-12 15:04:54 -0300

    feat: Add automatic login if there is token and all the paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,48 @@
-import {initAxiosInterceptors} from './Helpers/auth-helper'
-import {Switch, Route} from 'react-router-dom'
+import {React, useEffect, useState} from 'react'
+import {initAxiosInterceptors, getToken} from './Helpers/auth-helper'
+import {Switch, Route, Redirect} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import {setUser} from './redux/actions/userActions'
+import {whoami} from './Helpers/user-helper'
 import Register from './components/Register'
 import Login from './components/Login'
 import LandingPage from './components/LandingPage'
+import Home from './components/Home'
+import BudgetsView from './components/BudgetsView'
+import BudgetDetails from './components/BudgetDetails'
+import Nav from './components/Nav'
 
 function App() {
   initAxiosInterceptors()
+  const dispatch = useDispatch()
+
+  const [thereIsUser, setThereIsuser] = useState(false)
+
+  useEffect(() => {
+    async function setUserIfThereIsToken(){
+      if(getToken()){
+        let user = await whoami()
+        dispatch(setUser(user))
+        // Charge the store with the budgets here
+        setThereIsuser(true)
+      }
+    }
+    setUserIfThereIsToken()
+  }, [dispatch])
+
   return (
-    <Switch>
-      <Route exact path="/" component={LandingPage}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/register" component={Register}/>
-    </Switch>
+    <div>
+      {thereIsUser ? (<Nav />) : (null)}
+      <Switch>
+        {thereIsUser ? (<Redirect exact from="/" to="/home"/>) : (null)}
+        <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+        <Route exact path="/home" component={Home}/>
+        <Route exact path="/budgets" component={BudgetsView}/>
+        <Route exact path="/budgets/:id" component={BudgetDetails}/>
+      </Switch>
+    </div>
   );
 }
 
