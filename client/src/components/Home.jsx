28ee70703befb5903df3162800789e6306045794
Dany-Rolon <dany_rolon@outlook.com>
2021-05-12 16:09:20 -0300

    feat: Add BudgetCard to show budgets

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,30 @@
+import {useSelector} from 'react-redux'
+
 export default function Home(){
+
+    const budgets = useSelector(state => state.allBudgets.budgets)
+    console.log(budgets);
     return(
-        <div>
-            Home Page
-        </div>
+        budgets.map(item => 
+            <BudgetCard b={item} key={item.id}/>
+        )
     )
+
+    function BudgetCard({b}){ //b is for budget ;)
+        return(
+            <>
+                <h5>Concept</h5>
+                <p>{b.concept}</p>
+                <h5>Mount</h5>
+                <p>{b.mount}</p>
+                <h5>Type</h5>
+                <p>{b.type}</p>
+                <h5>Category</h5>
+                <p>{b.category}</p>
+                <h5>Date</h5>
+                <p>{b.date}</p>
+                <p>--------------------</p>
+            </>
+        )
+    }
 }
