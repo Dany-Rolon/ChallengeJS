b7054d252129a45d8438703a1f89778a42084200
Dany-Rolon <dany_rolon@outlook.com>
2021-05-13 16:30:13 -0300

    fix: Change Budget card to a component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,30 +1,13 @@
 import {useSelector} from 'react-redux'
+import BudgetCard from './BudgedCard'
 
 export default function Home(){
 
     const budgets = useSelector(state => state.allBudgets.budgets)
-    console.log(budgets);
+    // Tomar solo los primeros 10!!!
     return(
         budgets.map(item => 
             <BudgetCard b={item} key={item.id}/>
         )
     )
-
-    function BudgetCard({b}){ //b is for budget ;)
-        return(
-            <>
-                <h5>Concept</h5>
-                <p>{b.concept}</p>
-                <h5>Mount</h5>
-                <p>{b.mount}</p>
-                <h5>Type</h5>
-                <p>{b.type}</p>
-                <h5>Category</h5>
-                <p>{b.category}</p>
-                <h5>Date</h5>
-                <p>{b.date}</p>
-                <p>--------------------</p>
-            </>
-        )
-    }
 }
