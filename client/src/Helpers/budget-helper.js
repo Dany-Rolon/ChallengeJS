edbc458f3529ae78f4a3f38f233a32d494a66ba4
Dany-Rolon <dany_rolon@outlook.com>
2021-05-17 16:58:05 -0300

    feat: Add delete and edit functions

--- client/src/Helpers/budget-helper.js
+++ client/src/Helpers/budget-helper.js
@@ -14,7 +14,21 @@ async function postBudget(budget){
     }
 }
 
+async function deleteBudget(id){
+    const {status} = await axios.delete(`http://localhost:4000/budget/delete/${id}`)
+    if(status === 200){
+        return true
+    } else return false
+}
+
+async function editBudget(budget, id){
+    const result = await axios.put(`http://localhost:4000/budget/edit/${id}`, budget)
+    console.log('Edit budget', result)
+} 
+
 export {
     getBudgets,
-    postBudget
+    postBudget,
+    deleteBudget,
+    editBudget
 }
