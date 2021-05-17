3982212c0c88dda3aa05a655a6e93af480e2bda6
Dany-Rolon <dany_rolon@outlook.com>
2021-05-17 11:14:47 -0300

    feat: Create the budget card component
    Created with style and functionality

--- client/src/components/BudgedCard.jsx
+++ client/src/components/BudgedCard.jsx
@@ -1,17 +1,45 @@
-export default function BudgetCard({b}){ //b is for budget ;)
-    return(
-        <>
-            <h5>Concept</h5>
-            <p>{b.concept}</p>
-            <h5>Mount</h5>
-            <p>{b.mount}</p>
-            <h5>Type</h5>
-            <p>{b.type}</p>
-            <h5>Category</h5>
-            <p>{b.category}</p>
-            <h5>Date</h5>
-            <p>{b.date}</p>
-            <p>--------------------</p>
-        </>
+export default function BudgetCard({ b, options }) { //b is for budget ;)
+    return (
+        <div className="mx-14 sm:mx-10 bg-white">
+            <div className="px-4 pt-4">
+                <div className="grid grid-cols-1">
+                    {/* Concept */}
+                    <h5 className="text-xl font-bold text-bluegray-600">{b.concept}</h5>
+
+                    {/* Mount */}
+                    <div className="flex mt-2">
+                        <h5 className="font-semibold">Mount: </h5>
+                        <p className="pl-1 text-blue-400">${b.mount}</p>
+                    </div>
+
+                    {/* Type */}
+                    <div className="flex mt-2">
+                        <h5 className="font-semibold">Type: </h5>
+                        <p className={`${b.type === 'income' ? ('text-green-600') : ('text-red-500')} pl-1`}>{b.type}</p>
+                    </div>
+
+                    {/* Category */}
+                    <div className="flex mt-2">
+                        <h5 className="font-semibold">Category: </h5>
+                        <p className="pl-1">{b.category}</p>
+                    </div>
+
+                    {/* Date */}
+                    <div className="flex mt-2 mb-1">
+                        <h5 className="font-semibold">Date: </h5>
+                        <p className="pl-1">{b.date}</p>
+                    </div>
+                </div>
+            </div>
+            {/* Buttons */}
+            {
+                options ? (
+                    <div className="grid grid-cols-2 px-">
+                        <button className="block p-1 texet-center font-bold text-white bg-red-500 hover:bg-red-700">Delete</button>
+                        <button className="block p-1 texet-center font-bold text-white bg-blue-500 hover:bg-blue-700">Edit</button>
+                    </div>
+                ) : (null)
+            }
+        </div>
     )
 }
