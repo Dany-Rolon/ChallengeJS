af05742eef5f03cc343988cc34be80bd397f9f36
Dany-Rolon <dany_rolon@outlook.com>
2021-05-17 11:17:03 -0300

    feat: Show budgets in a grid

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,21 @@
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import BudgetCard from './BudgedCard'
+import CurrentBalance from './CurrentBalance'
 
-export default function Home(){
+export default function Home() {
 
     const budgets = useSelector(state => state.allBudgets.budgets)
     // Tomar solo los primeros 10!!!
-    return(
-        budgets.map(item => 
-            <BudgetCard b={item} key={item.id}/>
-        )
+    return (
+        <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 container mx-auto mb-16 mt-4 gap gap-y-4 ">
+                {
+                    budgets.map(item =>
+                        <BudgetCard b={item} key={item.id} />
+                    )
+                }
+            </div>
+            <CurrentBalance />
+        </div>
     )
 }
