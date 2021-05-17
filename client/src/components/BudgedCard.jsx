cd0d0e54ddfb96a9fe0a13d0eb7a6829f7f2f99e
Dany-Rolon <dany_rolon@outlook.com>
2021-05-17 16:53:35 -0300

    feat: Add delete and edit functionality

--- client/src/components/BudgedCard.jsx
+++ client/src/components/BudgedCard.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react"
+import {useDispatch} from 'react-redux' 
+import {useHistory} from 'react-router-dom'
+import {removeSelectedBudget, updateBalance, selectedBudget} from '../redux/actions/budgetsActions'
+import {deleteBudget} from '../Helpers/budget-helper'
+
 export default function BudgetCard({ b, options }) { //b is for budget ;)
+    const dispatch = useDispatch()
+    const [isDeleting, setIsDeleting] = useState(false)
+    const history = useHistory()
+    function onCancel(e) {
+        e.preventDefault()
+        setIsDeleting(false)
+    }
+
+    async function onDeleting(){
+        let ok = await deleteBudget(b.id)
+        if(ok){
+            dispatch(removeSelectedBudget(b.id))
+            dispatch(updateBalance())
+        }
+        setIsDeleting(false)
+    }
+
+    function onEdit(e){
+        e.preventDefault()
+        dispatch(selectedBudget(b))
+        history.push(`/budgets/${b.id}`)
+    }
+
     return (
         <div className="mx-14 sm:mx-10 bg-white">
             <div className="px-4 pt-4">
@@ -35,11 +64,24 @@ export default function BudgetCard({ b, options }) { //b is for budget ;)
             {
                 options ? (
                     <div className="grid grid-cols-2 px-">
-                        <button className="block p-1 texet-center font-bold text-white bg-red-500 hover:bg-red-700">Delete</button>
-                        <button className="block p-1 texet-center font-bold text-white bg-blue-500 hover:bg-blue-700">Edit</button>
+                        <button className="block p-1 texet-center font-bold text-white bg-red-500 hover:bg-red-700" onClick={() => setIsDeleting(true)}>Delete</button>
+                        <button className="block p-1 texet-center font-bold text-white bg-blue-500 hover:bg-blue-700" onClick={onEdit}>Edit</button>
                     </div>
                 ) : (null)
             }
+
+            {isDeleting ? (
+                <div className="grid grid-cols-1 justify-items-center place-content-center w-screen h-screen fixed top-0 left-0 bg-white bg-opacity-50">
+                    <div className="bg-warmgray-800 p-8 text-white">
+                        <h2 className="col-span-2 text-2xl text-center">Are you sure you want to delete this budget?</h2>
+                        <div className="grid grid-cols-2 mt-2">
+                            <button onClick={onCancel} className="rounded-tl-md rounded-bl-md text-center font-bold bg-blue-500 p-2 hover:bg-blue-700 focus:outline-none">Cancel</button>
+                            <button onClick={onDeleting} className="rounded-tr-md rounded-br-md text-center font-bold bg-red-500 p-2 hover:bg-red-700 focus:outline-none">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            ) : (null)}
+
         </div>
     )
 }
