f2b9f4df2612cc20606e0a8522c64a567de3a585
Dany-Rolon <dany_rolon@outlook.com>
2021-05-20 09:57:31 -0300

    fix: Show only 10 budgets in home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,8 +4,7 @@ import CurrentBalance from './CurrentBalance'
 
 export default function Home() {
 
-    const budgets = useSelector(state => state.allBudgets.budgets)
-    // Tomar solo los primeros 10!!!
+    const budgets = useSelector(state => state.allBudgets.budgets).slice(0,10)
     return (
         <div>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 container mx-auto mb-16 mt-4 gap gap-y-4 ">
